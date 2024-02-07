08cf71a61599aba42ec01f07f49436ae4f4f182f
Mateus.Viana <mateus.alves@areco.net.br>
2024-02-07 09:48:57 -0300

    FT: :tada: Initial Commit

---
+++ ExemploArquitetura.Infrastructure/ExemploInfraServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ExemploArquitetura.Infrastructure
+{
+    public static class ExemploInfraServices
+    {
+        /// <summary>
+        /// Adiciona os serviços de infraestrutura
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        public static void AddInfraServices(this IServiceCollection serviceProvider)
+        {
+            //Exemplo de como adicionar um serviço de infraestrutura
+            //serviceProvider.AddScoped<IExemploInfraService, ExemploInfraService>();
+        }
+    }
+}
---
+++ ExemploArquitetura/Pages/Exemplo/ExemploList.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ExemploArquitetura.Pages.Exemplo
+{
+    public partial class ExemploList
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; } = default!;
+
+        private string Teste { get; set; } = "Teste";
+
+        protected override void OnInitialized()
+        {
+            Teste = "Teste 2";
+        }
+    }
+}
